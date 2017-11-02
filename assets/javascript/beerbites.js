72ce8813c0b15bb65e9a1b49a6dab5ca636f3f4d
Branford Harris <branfordharris@Branfords-MacBook-Pro.local>
2017-11-02 17:49:41 -0500

    added searchbox functionality

--- assets/javascript/beerbites.js
+++ assets/javascript/beerbites.js
@@ -19,19 +19,20 @@ function getFoodPairing(){
 
 }//end of getFoodPairing()
 
-getFoodPairing();
+// getFoodPairing();
 
 
 $("#search").click(function(){
 	searchBeer();
+
 
 });
 
 
 
 function searchBeer() {
-        var beer = $("#search").val().trim();
-        var queryURL = proxy + "https://api.brewerydb.com/v2/search?q=bud+light&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json";
+        var beer = $("#form1").val().trim();
+        var queryURL = proxy + "https://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json";
         
         $.ajax({
           url: queryURL,
@@ -42,7 +43,7 @@ function searchBeer() {
         }).done(function(response) {
           console.log(beer);
           console.log(response.data[0].name);
-          console.log(response.data[0].style.category.name);
+          console.log(response.data[0].style.shortName);
         });
 
 }
