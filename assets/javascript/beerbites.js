550541f5879650a022af54e48a4685d2030b655e
Branford Harris <branfordharris@Branfords-MacBook-Pro.local>
2017-11-01 21:10:26 -0500

    added BeerDB api

--- assets/javascript/beerbites.js
+++ assets/javascript/beerbites.js
@@ -1,15 +1,15 @@
-// function searchBeer() {
+function searchBeer() {
 
-//         var beer = $(this).attr("data-name");
-//         var queryURL = "https://api.brewerydb.com/v2/search?q=" + "bud+light" + "&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json
-// ";
+        var beer = $(this).attr("data-name");
+        var queryURL = "https://api.brewerydb.com/v2/search?q=" + "bud+light" + "&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json
+";
 
-//         // Creates AJAX call for the specific movie button being clicked
-//         $.ajax({
-//           url: queryURL,
-//           method: "GET"
-//         }).done(function(response) {
-//           console.log(response)
-//         };
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response.data.name);
+        };
 
-// }
+}
