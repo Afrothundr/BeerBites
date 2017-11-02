1a9416c023f2e578f12f522016c80520f4ab6670
Branford Harris <branfordharris@Branfords-MacBook-Pro.local>
2017-11-01 21:29:56 -0500

    working on console logging api call

--- assets/javascript/beerbites.js
+++ assets/javascript/beerbites.js
@@ -1,15 +1,27 @@
+
+
+
+
+
+
+
+
+
+$("#search").click(function(){
+	searchBeer();
+});
+
+
+
 function searchBeer() {
-
-        var beer = $(this).attr("data-name");
-        var queryURL = "https://api.brewerydb.com/v2/search?q=" + "bud+light" + "&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json
-";
-
-        // Creates AJAX call for the specific movie button being clicked
+        var beer = $("#search").val().trim();
+        var queryURL = "https://api.brewerydb.com/v2/search?q=bud+light&type=beer&key=0191c57ff93f0b5868e91f7e67f611e7&format=json";
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
+          console.log(beer);
           console.log(response.data.name);
-        };
+        });
 
 }
